2b400ec5113a66cb5465951ac94d082d0e9956b5
rmoergeli <rene.moergeli@windowslive.com>
2020-05-23 11:33:01 +0200

    Add project files.

---
+++ BlazorAppTest2/Controllers/StudentsController.cs
@@ -0,0 +1,30 @@
+﻿using BlazorAppTest2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorAppTest2.Controllers
+{
+    public class StudentsController : Controller
+    {
+        [ActionName("Index")]
+        public IActionResult Index()
+        {
+            return View(GetStudents());
+        }
+
+        private List<Student> GetStudents()
+        {
+            List<Student> students = new List<Student>();
+            students.Add(new Student
+            {
+                Name = "Smith",
+                Surname = "John"
+            });
+
+            return students;
+        }
+    }
+}
